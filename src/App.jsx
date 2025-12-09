2edfff696bf48156f5bb30d3c2434126660bb453
Rohit <rohitghadge348@gmail.com>
2025-12-09 22:14:56 +0530

    work started

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { createBrowserRouter } from 'react-router-dom'
+import './App.css'
+
+createBrowserRouter([
+  {},
+  {}
+])
+
+function App() {
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
