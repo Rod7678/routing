afa611c193e6c5c78436f196058e1e5376e934ed
Rohit <rohitghadge348@gmail.com>
2025-12-10 20:46:16 +0530

    add some nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
-import HomePage from './pages/Home'
-import Products from './pages/Products'
+import HomePage from './pages/Home.jsx'
+import Products from './pages/Products.jsx'
+import RootLayout from './pages/Root.jsx'
+import ErrorPage from './pages/Error.jsx'
 
-const router = createBrowserRouter([
-  { path: '/', element: <HomePage />},
-  { path: '/products', element: <Products/> }
+const router = createBrowserRouter([  
+  { path: '/',
+    element: <RootLayout/>,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: '/', element: <HomePage />},
+      { path: '/products', element: <Products/> }
+    ] 
+   },
 ])
 function App() {
   return (
---
+++ src/components/MainNavigation.jsx
@@ -0,0 +1,29 @@
+import { NavLink } from "react-router-dom";
+import './MainNavigation.css'
+
+
+export default function MainNavigation () {
+    return(
+        <>
+        <header className="header">
+            <nav>
+                <ul className="list">
+                    <li>
+                        <NavLink 
+                        to='/'
+                        className={({isActive}) => isActive ? 'active': undefined}
+                        end
+                        >Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/products'
+                        className={({isActive}) => isActive ? 'active': undefined}
+                        >Products</NavLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        </>
+    )
+}
