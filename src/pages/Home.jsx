5cb53649dcefe1d360996bbf0cc8c90836bdce6a
Rohit <rohitghadge348@gmail.com>
2025-12-11 14:05:06 +0530

    navigating programatically

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function HomePage(){
+    const navigate = useNavigate();
+    function useNavigateHandler(){
+        setTimeout(()=>{
+            navigate('/products')
+        },1000)
+    }
+
     return (
         <>
         <h2>my home Page</h2>
         <p>
             Go to <Link to='/products'>products page</Link>
         </p>
+        <button onClick={useNavigateHandler}>navigate to product page with 1s </button>
         </>
     )
 }
