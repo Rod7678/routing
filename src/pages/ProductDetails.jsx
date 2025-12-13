74d7bd4793a0697823322d6bd6c30681352fd761
Rohit <rohitghadge348@gmail.com>
2025-12-13 13:31:26 +0530

    add relative path

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 
 export default function ProducDetails() {
     const params = useParams();
@@ -6,6 +6,7 @@ export default function ProducDetails() {
     return (
         <>
         <h1>product Detail of {params.productId}</h1>
+        <p><Link to=".." relative="path">Back</Link></p>
         </>
     )
 }
